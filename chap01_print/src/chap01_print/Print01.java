80d8128d09f74b5803c1de51c713b66dde8430a2
wig7227 <wig7227@naver.com>
2024-12-23 16:03:26 +0900

    1223_3교시

---
+++ chap01_print/src/chap01_print/Print01.java
@@ -0,0 +1,26 @@
+package chap01_print;
+
+public class Print01 {
+
+	public static void main(String[] args) {
+		//한줄 주석 : 컴파일이 되지 않는다 (실행이 되지 않는다)
+		//		한줄 주석의 단축키 ( ctrl + / )
+		
+		/*
+		   ddddddd 단축키 : 블럭을 잡고 ctrl + shift + /
+		 */
+		/* 여러줄 주석
+		 * 
+		 *  
+		 *  */
+		// 화면에 출력해주는 프로그램
+		System.out.println("본인이름 : 김태형"); //System 앞에는 무조건 대문자, 문자열은 쌍따음표
+		System.out.println("더조은 아카데미 학원"); //ㅣn 은 line 이라는뜻 (출력후 줄바꿈 하시오)
+		System.out.println("안녕하세요");
+		System.out.println("강남에서 제일 좋은 학원");
+		System.out.println("안녕히가세요");
+		
+		
+	}
+
+}
